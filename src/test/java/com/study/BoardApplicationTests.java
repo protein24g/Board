d45e2f1cf63af4f979d15d3de0df2bcd09ca7179
PC <dns6574@naver.com>
2024-04-12 22:52:42 +0900

    Changes

--- src/test/java/com/study/BoardApplicationTests.java
+++ src/test/java/com/study/BoardApplicationTests.java
@@ -2,15 +2,11 @@ package com.study;
 
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
-import com.study.user.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootTest
 public class BoardApplicationTests {
